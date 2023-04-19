8885519a776030e179e98581b76192515492083c
Евгений Маликов <malikov.evgeniy@gmail.com>
2023-04-19 22:53:28 +0300

    Mixins - убираем дублирование кода

--- pokemon/templatetags/pokemon_tags.py
+++ pokemon/templatetags/pokemon_tags.py
@@ -1,29 +1,29 @@
-from django import template
-from pokemon.models import *
-
-register = template.Library()
-
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
-
-
-@register.simple_tag(name='getcats')
-def get_categories(filter=None):
-    if not filter:
-        return Category.objects.all()
-    else:
-        return Category.objects.filter(pk=filter)
-
-
-@register.inclusion_tag('pokemon/list_categories.html')
-def show_categories(sort=None, cat_selected=0):
-    if not sort:
-        cats = Category.objects.all()
-    else:
-        cats = Category.objects.order_by(sort)
-
-    return {"cats": cats, "cat_selected": cat_selected}
-
-
-@register.inclusion_tag('pokemon/menu.html')
-def show_menu():
-    return {'menu': menu}
+# from django import template
+# from pokemon.models import *
+#
+# register = template.Library()
+#
+# menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+#
+#
+# @register.simple_tag(name='getcats')
+# def get_categories(filter=None):
+#     if not filter:
+#         return Category.objects.all()
+#     else:
+#         return Category.objects.filter(pk=filter)
+#
+#
+# @register.inclusion_tag('pokemon/list_categories.html')
+# def show_categories(sort=None, cat_selected=0):
+#     if not sort:
+#         cats = Category.objects.all()
+#     else:
+#         cats = Category.objects.order_by(sort)
+#
+#     return {"cats": cats, "cat_selected": cat_selected}
+#
+#
+# @register.inclusion_tag('pokemon/menu.html')
+# def show_menu():
+#     return {'menu': menu}
--- pokemon/views.py
+++ pokemon/views.py
@@ -1,39 +1,86 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import redirect, render, get_object_or_404
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import AddPostForm
 from .models import *
+from .utils import DataMixin
 
 
-class PokemonHome(ListView):
+class PokemonHome(DataMixin, ListView):
     model = Pokemon
     template_name = 'pokemon/index.html'
     context_object_name = 'posts'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Главная страница'
+        c_def = self.get_user_context(title="Главная страница")
+        context = dict(list(context.items()) + list(c_def.items()))
         return context
 
     def get_queryset(self):
         return Pokemon.objects.filter(is_published=True)
 
 
-class PokemonCategory(ListView):
+class PokemonCategory(DataMixin, ListView):
     model = Pokemon
-    template_name = 'Pokemon/index.html'
+    template_name = 'pokemon/index.html'
     context_object_name = 'posts'
     allow_empty = False
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Категория - ' + str(context['posts'][0].cat)
-        context['cat_selected'] = context['posts'][0].cat_id
-        return context
+        c_def = self.get_user_context(title='Категория - ' + str(context['posts'][0].cat),
+                                      cat_selected=context['posts'][0].cat_id)
+
+        return dict(list(context.items()) + list(c_def.items()))
 
     def get_queryset(self):
         return Pokemon.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
+
+
+class AddPage(LoginRequiredMixin, DataMixin, CreateView):
+    form_class = AddPostForm
+    template_name = 'pokemon/addpage.html'
+    success_url = reverse_lazy('home')
+    login_url = reverse_lazy('home')
+    raise_exception = True
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Добавление статьи")
+        context = dict(list(context.items()) + list(c_def.items()))
+        return context
+
+
+class ShowPost(DataMixin, DetailView):
+    model = Pokemon
+    template_name = 'pokemon/post.html'
+    slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title=context['post'])
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+
+def login(request):
+    return HttpResponse("Авторизация")
+
+
+def about(request):
+    return render(request, 'pokemon/about.html', {'title': 'О сайте'})
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
 
 def show_category(request, cat_id):
@@ -48,33 +95,3 @@ def show_category(request, cat_id):
     }
 
     return render(request, 'pokemon/index.html', context=context)
-
-
-class ShowPost(DetailView):
-    model = Pokemon
-    template_name = 'pokemon/post.html'
-    slug_url_kwarg = 'post_slug'
-    context_object_name = 'post'
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = context['post']
-        return context
-
-
-class AddPage(CreateView):
-    form_class = AddPostForm
-    template_name = 'pokemon/addpage.html'
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Добавление статьи'
-        return context
-
-
-def about(request):
-    return render(request, 'pokemon/about.html', {'title': 'О сайте'})
-
-
-def pageNotFound(request, exception):
-    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
